8c56c73ec0911d1eb165f4a0ceca55713b0974ae
Jaromír Beneš <jaromir.benes@gmail.com>
2023-05-30 16:41:33 +0500

    Information set

--- simulate_export_productivity.m
+++ simulate_export_productivity.m
@@ -39,19 +39,34 @@ t1 = table( ...
 
 d = steadydb(m0, 1:40);
 
+
 %% Simulate model with higher productivity
 
+% Unexpected, from time t=3
 d1 = d;
 s1 = simulate( ...
-    m1, d1, 1:40 ...
+    m1, d1, 3:40 ...
     , "prependInput", true ...
     , "method", "stacked" ...
+);
+
+
+p2 = Plan.forModel(m1, 1:40);
+p2 = swap(p2, 1:2, ["Az", "shk_Az"]);
+
+d2 = d;
+s2 = simulate( ...
+    m1, d2, 1:40 ...
+    , "prependInput", true ...
+    , "method", "stacked" ...
+    , "plan", p2 ...
 );
 
 
 %% Create simulation minus control databanks
 
 smc1 = databank.minusControl(m1, s1, d);
+smc2 = databank.minusControl(m1, s2, d);
 
 
 %% Chart results
@@ -60,10 +75,12 @@ ch = databank.Chartpack();
 ch.Range = 0:40;
 ch.PlotSettings = {"marker", "s"};
 
-ch + ["Copt", "Chtm", "Id", "Kz", "Iz"];
+ch + ["Az", "Copt", "Chtm", "Id", "Kz", "Iz"];
 ch + ["S", "dPc", "Rg", "W/Pc"];
 ch + ["NIP_NGDP", "CA_NGDP", "TB_NGDP"];
 
-draw(ch, smc1);
+draw(ch, databank.merge("horzcat", smc1, smc2));
+
+visual.hlegend("bottom", "Unexpected", "Expected");
 
 
