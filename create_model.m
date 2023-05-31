6fb786b28b2737cced548790779f116be4817aa2
Jaromír Beneš <jaromir.benes@gmail.com>
2023-05-31 20:48:15 +0500

    Update from develop branch

--- create_model.m
+++ create_model.m
@@ -1,4 +1,3 @@
-
 %% Read model with primary and non-primary exports
 
 
@@ -58,9 +57,9 @@ m.rho_Pw_star = 0.3;
 
 % Steady-state parameters
 
-m.beta = 0.95;
-m.delta_Kd = 0.20;
-m.delta_Kh = 0.20;
+m.ss_dA = 1.04;
+m.ss_dPc = 1.05;
+m.beta = 0.95; 
 
 
 % Households
@@ -76,19 +75,20 @@ m.chi_dli = 0;
 m.chi_c = 0.5;
 m.nu = 0;
 
+m.beta_Kd = 0.9;
+m.delta_Kd = 0.10;
+m.delta_Kh = 0.10;
 
-
-m.ss_TFwh_NGDP = 0.12;
+m.ss_TFwh_NGDP = 0.12; % to be reverse engineered
 
 % Markups
 m.mu_CI = 1.25; % to be reverse-engineered 
 m.mu_D2 = 1; % to be reverse-engineered 
 
 % Local production
-m.gamma_Md = 0.45; 0.3792; 0.30; % to be reverse-engineered 
-m.gamma_Nd = 0.45; 0.37; % to be reverse-engineered 
-%m.ss_N0d = 0.65; % to be reverse-engineered 
-m.ss_N0d_Nd = 0.30;
+m.gamma_Md = 0.40; 0.3792; 0.30; % to be reverse-engineered 
+m.gamma_Nd = 0.50; % to be reverse-engineered 
+m.ss_N0d_Nd = 0.35;
 
 % Interest rate premia
 m.zeta_Rg0 = 0; % to be reverse-engineered 
@@ -101,12 +101,10 @@ m.zeta_S = 0.5;
 % Directly calibrated ratios and rates
 
 % Residential investment
-m.ss_Kh_Kd = 0.10; 0.30; % to be reverse-engineered 
+m.ss_Kh_Kd = 0.10; % to be reverse-engineered 
 m.lambda_Ih1 = 0.05;
 
 % Steady state for exogenous variables
-m.ss_dA = 1.04;
-m.ss_dPc = 1.05;
 m.ss_Ad = 1;
 
 % Transitory parameters
@@ -129,10 +127,10 @@ m.xi_NNd = 0;
 
 % __Primary export sector (Q)__
 
-% Steady state for exogenous/external/policy variables
+% Steady State for Exogenous/External/Policy Variables
 m.ss_Aq = 1;
 m.ss_Pq_Pw = 1;
-m.gamma_Mq = 0.30; 0.25;
+m.gamma_Mq = 0.25; % if non-primary exporters disabled, use m.gamma_Mq = 0.29 in read_model
 m.gamma_J = 0.07;
 
 % Autoregression Parameters
@@ -142,8 +140,8 @@ m.rho_Pq_Pw = 0.5;
 
 % __Energy sector (J)__
 
-m.lambda_Pj0 = 0.5;
-m.mu_J = 1;
+m.gamma_Mj = 0.25;
+m.lambda_Pj0 = 1;
 
 
 % __Nonprimary export (Z)__
@@ -153,10 +151,9 @@ m.mu_J = 1;
 % Non-Primary Exports
 m.mu_Z = 1; % to be reverse-engineered 
 m.gamma_Kz = 0.2;
-m.gamma_Mz = 0.50;0.40;
+m.gamma_Mz = 0.40;
 m.gamma_Nz = 0.45;
-m.gamma_N0z = 0.4;
-m.delta_Kz = 0.20;
+m.delta_Kz = 0.10;
 m.ss_N0z_Nz = 0.30;
 
 % Steady State for Exogenous/External Variables
@@ -181,15 +178,17 @@ m.lambda_Z_ref = 0.7;
 m.xi_NNz = 0;
 m.xi_Z = 0.3;
 
-m.psi_jw = 0;
-m.psi_qw = 0;
+% Ownership of NP exporters. Note: local HH could also hold equity, i.e.
+% psi_zw + psi_zg do not need to sum up to 1
+m.psi_zw = 0; % share of foreign-owned equity in nonprimary export sectors
+m.psi_zg = 0; % share of PIF-owned equity nonprimary export sectors
 
 
 % __Monetary policy__
 
 m.rho_Rg = 0.5;
 m.kappa_dPc = 3;
-m.kappa_dS = 1;
+m.kappa_dS = 0;1;
 
 
 % __Fiscal__
@@ -202,12 +201,11 @@ m.ss_Bgw_Bg = 0.85;
 
 % Government transfers to households
 m.ss_TFgh_NGDP = 0.05;
+m.ss_TFgj_NGDP = 0.01;
 
 % Government consumption
-m.ss_PcG_NGDP = 0.26;
+m.ss_PcG_NGDP = 0.16;
 m.ss_WNg_NGDP = 0.10;
-
-m.omega = 0.30;
 
 % Government investment
 m.ss_PiIg_NGDP = 0.08;
@@ -223,8 +221,11 @@ m.ss_Bwf_NGDP = 0.10;
 % Tax rates
 m.ss_TRvat = 0; % to be reverse-engineered 
 m.ss_TRlit = 0.03;
-
 m.ss_TRgd = 0;
+m.ss_TRj = 0;
+m.ss_TRq = 0;
+
+m.ss_TAXls_NGDP = 0; %%%0.05; % to be reverse-engineered
 
 % Dynamic parameters
 
@@ -233,6 +234,9 @@ m.lambda_Gg2 = 0.5;
 
 m.lambda_Ng1 = 0.2;%0.1;
 m.lambda_Ng2 = 0.5; 
+
+m.lambda_TAXls1 = 0.5;
+m.lambda_TAXls2 = 0.5;
 
 m.lambda_Ig1 = 1.5;%0.5; 
 
@@ -244,9 +248,17 @@ m.lambda_BCBg = 0.5;
 
 % __Labor__
 
+% Steady state parameters
+
+m.omega = 0.30;
+m.ss_Whtm_Wopt = 1; %%% 0.45;
+
+m.eta = 0;
+
 % Dynamic parameters
 
-m.rho_W = 0.4;
+m.rho_Wopt = 0.4;
+m.rho_Whtm = 0.4;
 
 
 
@@ -265,18 +277,11 @@ m = steady( ...
 );
 
 checkSteady(m);
-m = solve(m)
 
-save mat/create_model.mat m
-return
+% return
 
 
-%% Directly calibrate Uzbeksitan steady state
-
-m.ss_TFwh_NGDP = 0.12;
-
-
-%% Reverse engineer Uzbekistan steady state
+%% Reverse engineer parameters for steady-state ratios
 
 swap = string.empty(0, 2);
 
@@ -292,13 +297,17 @@ swap = [swap; "PiIg_NGDP", "ss_Kg_A"];
 m.PqQ_NGDP = 0.08;
 swap = [swap; "PqQ_NGDP", "ss_Aq"];
 
-% Energy volume
-m.PjJ_NGDP = 0.04;
-swap = [swap; "PjJ_NGDP", "gamma_J"];
+% Energy cost base
+m.Pj0J_NGDP = 0.04;
+swap = [swap; "Pj0J_NGDP", "gamma_J"];
 
-% Import for energy sector
-m.TAXj_NGDP = 0.03;
-swap = [swap; "TAXj_NGDP", "psi_jw"];
+% Energy sector tax
+m.TAXj_NGDP = 0.02;
+swap = [swap; "TAXj_NGDP", "ss_TRj"];
+
+% Primary export sector
+m.TAXq_NGDP = 0.07;
+swap = [swap; "TAXq_NGDP", "ss_TRq"];
 
 % Value added tax
 m.TAXvat_NGDP = 0.07;
@@ -313,7 +322,10 @@ m.TFgd_NGDP = 0.01;
 swap = [swap; "TFgd_NGDP", "ss_TRgd"];
 
 
+%% Calibrate staedy state
+
 % Fix unit root processes
+
 
 m.A = 1;
 m.Pw_star = 1;
@@ -338,6 +350,8 @@ t = table( ...
 disp(t)
 
 m = solve(m);
+
+disp DONE
 
 save mat/create_model.mat m
 
